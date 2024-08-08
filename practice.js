94d214de59182c63ab969dc9492a8ecca8713028
secret valley <88777641+rsfsrg@users.noreply.github.com>
2024-08-08 16:04:22 +0530

    Create practice.js
    practice problem

---
+++ practice.js
@@ -0,0 +1,8 @@
+//practice question
+//for a given array with marks of student -> [85,97,44,37,76,60]
+//find the average markas of the entire class
+let marks = [85,97,44,37,76,60];
+
+let sum = 85+97+44+37+76+60;
+
+console.log("average marks of class =", sum / marks.length );
